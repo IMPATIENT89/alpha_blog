b1d87652e56a80b493571afe4f3127f79a06a9a8
ANKIT SHUKLA <ankitshukla8948328509@gmail.com>
2022-05-24 00:07:53 +0530

    Refactor code using partials and make app production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]    In this line we are using all of the CRUD action so we can just simply write like below
+  resources :articles
 end
